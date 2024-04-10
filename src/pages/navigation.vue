89b45e32876b2212fea6c3995c6547442dd8bc8d
F_coder <123@123.com>
2024-04-10 15:57:57 +0800

    fix nav image

--- src/pages/navigation.vue
+++ src/pages/navigation.vue
@@ -2,7 +2,7 @@
 import {themeStore} from '@/stores/menu';
 import SvgIcon from "@/components/SvgIcon.vue";
 import {getNavWebsite} from "@/request/api/nav.js";
-
+import img from '/favicon.ico'
 const themeData = themeStore()
 
 //语言
@@ -11,7 +11,7 @@ let {theme} = storeToRefs(themeData);
 let navData = ref({})
 // let outFlyArr=ref(['animate__animated','animate__fadeOutTopRight'])
 
-let emptyImage=ref ("/favicon.ico")
+let emptyImage=ref (img)
 
 //设置图标
 function renderIcon(icon) {
@@ -153,7 +153,7 @@ const redirectToExternalSite = (index, url) => {
   display: flex;
   align-items: center !important;
   flex-direction: row;
-  margin-top: 20px;
+  margin-bottom: 20px;
   box-shadow: rgba(17, 17, 26, 0.05) 0px 1px 0px, rgba(17, 17, 26, 0.1) 0px 0px 8px;
   border-radius: 10px;
 }
